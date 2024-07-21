84818a6313a745354a1dceb92dab9cbfaf57e096
해준 <127281057+ftery0@users.noreply.github.com>
2024-07-21 20:29:36 +0900

    프로젝트 세팅

---
+++ packages/foundation/color/type.ts
@@ -0,0 +1,45 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface AlimoThemeType {
+    Color: {
+      Natural00: string;
+      Natural10: string;
+      Natural20: string;
+      Natural30: string;
+      Natural40: string;
+      Natural50: string;
+      Natural60: string;
+      Natural70: string;
+      Natural80: string;
+      Natural90: string;
+      Natural100: string;
+
+      Primary00: string;
+      Primary10: string;
+      Primary20: string;
+      Primary30: string;
+      Primary40: string;
+      Primary50: string;
+      Primary60: string;
+      Primary70: string;
+      Primary80: string;
+      Primary90: string;
+      Primary100: string;
+
+      Red00: string;
+      Red10: string;
+      Red20: string;
+      Red30: string;
+      Red40: string;
+      Red50: string;
+      Red60: string;
+      Red70: string;
+      Red80: string;
+      Red90: string;
+      Red100: string;
+
+      Common: string;
+    };
+  }
+}
