f5f33fd575277f0f57524b38a029cecfb6ea5251
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-07-30 22:29:12 +0900

    refactor :: Buton stories

--- packages/components/ui/Button/Button.stories.tsx
+++ packages/components/ui/Button/Button.stories.tsx
@@ -7,33 +7,12 @@ export default {
   args: {
     children: "로그인",
   },
-  tags: ["autodocs"]
+  tags: ["autodocs"],
 } as Meta<typeof Button>;
 
-export const CtaButton: StoryObj<typeof Button> = {
+export const Common: StoryObj<typeof Button> = {
   args: {
     disabled: false,
     buttonSize: "cta",
-  },
-};
-
-export const LargeButton: StoryObj<typeof Button> = {
-  args: {
-    disabled: false,
-    buttonSize: "large",
-  },
-};
-
-export const MediumButton: StoryObj<typeof Button> = {
-  args: {
-    disabled: false,
-    buttonSize: "medium",
-  },
-};
-
-export const SmallButton: StoryObj<typeof Button> = {
-  args: {
-    disabled: false,
-    buttonSize: "small",
   },
 };
