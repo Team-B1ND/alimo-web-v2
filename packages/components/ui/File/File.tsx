fde49ac5d1c4e01ddf75ede1620c4ad556091ba1
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-08-09 20:51:29 +0900

    feat :: icon 폴더 위치 변경

--- packages/components/ui/File/File.tsx
+++ packages/components/ui/File/File.tsx
@@ -4,8 +4,8 @@ import * as S from "./style";
 import { FileProps } from "./types";
 import { AlimoColor } from "../../../foundation/src/token/colorToken";
 import { AlimoThemelight } from "../../../styles/src/AlimoTheme/AlimoTheme";
-import FileImg from "../../../icons/src/File";
-import DownLoadImg from "../../../icons/src/expand/ExpandStopDown";
+import FileImg from "../../../assets/src/icons/src/File";
+import DownLoadImg from "../../../assets/src/icons/src/expand/ExpandStopDown";
 
 export const File = ({ size, title, width, onclick }: FileProps) => {
   return (
