09c3943d78c8200473f9b410f96facd54a4d6e11
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-08-13 12:41:46 +0900

    setting :: write #8

---
+++ apps/admin/src/components/write/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@alimo/components/ui/Button/Button";
+import React from "react";
+
+const Write = () => {
+  return (
+    <div>
+      <Button disabled={false} onclick={() => {}} buttonSize="cta">
+        adsf
+      </Button>
+    </div>
+  );
+};
+
+export default Write;
